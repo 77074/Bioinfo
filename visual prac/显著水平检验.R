3df6173aea713e88392054cef9bd386a69144b0f
77074 <108330504+77074@users.noreply.github.com>
2024-03-02 13:46:57 +0800

    Delete visual prac directory

--- visual prac/显著水平检验.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2) # R语言中最常用的基于grid的可视化工具
-library(RColorBrewer)
-library(ggpubr)
-
-
-ggboxplot(ToothGrowth,x="dose",y="len",color="dose")+
-  stat_compare_means(method="anova",label.y=40)+
-  stat_compare_means(label ="p.signif",method = "t.test",ref.group = ".all.") # signif 去掉数值，.all.与总体进行比较
